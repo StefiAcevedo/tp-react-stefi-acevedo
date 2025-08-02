5da8ae5f06d01ca618b7780dbde27e19162880d3
CALE GROUP <calegroup@CALEs-MBP.fibertel.com.ar>
2025-08-02 17:09:16 -0300

    ajustes en estilos del navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,52 @@
+import React, { useState } from 'react';
 import './NavBar.css';
 import { NavLink } from 'react-router-dom';
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="navbar-list">
-        <li><NavLink to="/" className="navbar-link" end>Inicio</NavLink></li>
-        <li><NavLink to="/populares" className="navbar-link">Populares</NavLink></li>
-        <li><NavLink to="/buscador" className="navbar-link">Buscador</NavLink></li>
-      </ul>
+      <div className="navbar-container">
+        <div className="navbar-logo">Mi App</div>
+
+        <button
+          className={`navbar-toggle ${menuOpen ? 'open' : ''}`}
+          onClick={toggleMenu}
+          aria-label="Toggle navigation menu"
+          aria-expanded={menuOpen}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+
+        <ul className={`navbar-list ${menuOpen ? 'active' : ''}`}>
+          <li>
+            <NavLink to="/" className="navbar-link" end onClick={closeMenu}>
+              Inicio
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/populares" className="navbar-link" onClick={closeMenu}>
+              Populares
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/buscador" className="navbar-link" onClick={closeMenu}>
+              Buscador
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
