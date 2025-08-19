706a1cd8d9abe9d7a20490c648d23d69774352cb
CALE GROUP <calegroup@CALEs-MBP.fibertel.com.ar>
2025-08-19 17:11:32 -0300

    cambios en router.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,22 +1,25 @@
+// router
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Populares from '../pages/Populares';
 import Buscador from '../pages/Buscador';
 import Detalle from '../pages/Detalle';
-import NavBar from '../components/NavBar'; // navbar
+import UltimosLanzamientos from '../pages/UltimosLanzamientos';
+import NavBar from '../components/NavBar';
 
-function AppRouter() {
+function Router() {
   return (
     <BrowserRouter>
       <NavBar />
-      <Routes> {/* Acá debo ir sumando las diferentes rutas: controlar el Mayus*/}
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/populares" element={<Populares />} />
         <Route path="/buscador" element={<Buscador />} />
         <Route path="/detalle/:id" element={<Detalle />} />
+        <Route path="/ultimos-lanzamientos" element={<UltimosLanzamientos />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-export default AppRouter;
+export default Router;
